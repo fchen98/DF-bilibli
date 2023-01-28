4d5245e6b767b47a02c53e2bd74dd7afd7857654
fchen98 <43590246+fchen98@users.noreply.github.com>
2023-01-27 20:17:01 -0500

    Update script.js
    change the background when the dom is fully loaded, so it shows the wallpaper instead of null

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 function homeClear(){
-    var banner = document.getElementsByClassName("bili-banner")[0].cloneNode;
-    banner.classList.remove('bili-banner');
-    banner.classList.add('bili-wall');
-    banner.style.backgroundImage = 'url("https://raw.githubusercontent.com/fchen98/DF-bilibli/main/Bilibili_logo_bar.png")';
-    banner.style.backgroundColor = 'white';
-    banner.parentNode.appendChild(banner);
-    var clone = banner.cloneNode(true);
-    clone.getElementsByClassName("taper-line")[0].remove();
-    for(var i=0;i<4;i++){
-      banner.parentNode.appendChild(clone.cloneNode(true));
-  
-    }
-  }
+    var targetDiv, banner;
+    // wait until DOM is fully loaded
+    var intervalID = setInterval(function() {
+        targetDiv = document.getElementsByClassName("bili-banner")[0];
+        if (targetDiv != null) {
+
+            banner = targetDiv.cloneNode(true);
+            banner.className += ' bili-wall';
+            banner.style.backgroundImage = 'url("https://raw.githubusercontent.com/fchen98/DF-bilibli/main/Bilibili_logo_bar.png")';
+            banner.style.backgroundColor = 'white';
+            document.getElementsByClassName("bili-banner")[0].parentNode.appendChild(banner);
+            var clone = banner.cloneNode(true);
+            clone.getElementsByClassName("taper-line")[0].remove();
+            for(var i=0;i<4;i++){
+            banner.parentNode.parentNode.appendChild(clone.cloneNode(true));
+            }
+            
+            clearInterval(intervalID);
+        }
+    }, 2000);
   
   homeClear();
